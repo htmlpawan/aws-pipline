0f3e525694098ca3e2883a203fabdcb3ef2af8d7
Pawan kumar <ypawan80@gmail.com>
2024-05-27 17:28:11 +0530

    updated 2

--- dbcon.php
+++ dbcon.php
@@ -11,7 +11,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully pawan";
+echo "Connected successfully ";
 
 // Assuming $conn is already established from previous code
 
